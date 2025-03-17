43ca8a8c80b8352408019a14c679c149e595f57d
MarmureanuVlad <vlad.marmureanu@gmail.com>
2025-03-17 19:54:02 +0200

    Seminar 3

---
+++ S3_SDD.c
@@ -0,0 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+
+void afisareMasina(Masina masina) {
+	//afiseaza toate atributele unei masini
+	printf("Id: %d\n", masina.id);
+	printf("Nr usi: %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareVectorMasini(Masina* masini, int nrMasini) {
+	//afiseaza toate elemente de tip masina din vector
+	//prin apelarea functiei afisareMasina()
+	for (int i = 0; i < nrMasini; i++)
+	{
+		afisareMasina(masini[i]);
+	}
+}
+
+void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
+	//adauga in vectorul primit o noua masina pe care o primim ca parametru
+	//ATENTIE - se modifica numarul de masini din vector;
+	Masina* aux = (Masina*)malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	for (int i = 0; i < *nrMasini; i++)
+	{
+		aux[i] = (*masini)[i];
+	}
+	aux[*nrMasini] = masinaNoua;
+	(*nrMasini)++;
+	free(*masini);
+	(*masini) = aux;
+}
+
+Masina citireMasinaFisier(FILE* file) {
+	//functia citeste o masina dintr-un strceam deja deschis
+	//masina citita este returnata;
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	//aux = strtok(buffer, sep);
+	m1.id = atoi(strtok(buffer, sep));
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep); //dereferentiere
+
+	return m1;
+}
+
+Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaFisier()
+	//numarul de masini este determinat prin numarul de citiri din fisier
+	//ATENTIE - la final inchidem fisierul/stream-ul
+
+	FILE* file = fopen(numeFisier, "r");
+	Masina* masini = NULL;
+
+	do {
+		adaugaMasinaInVector(&masini, nrMasiniCitite, citireMasinaFisier(file));
+	} while (!feof(file));
+
+	fclose(file);
+	return masini;
+}
+
+void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
+	//este dezalocat intreg vectorul de masini
+	for (int i = 0; i < nrMasini; i++) {
+		free((*vector)[i].model);
+		free((*vector)[i].numeSofer);
+	}
+	free(*vector);
+	*vector = NULL;
+	*nrMasini = 0;
+}
+
+float pretMediuDupaNrUsi(Masina* vector, int nrMasini, int nrUsi) {
+	float suma = 0;
+	int index = 0;
+	for (int i = 0; i < nrMasini; i++) {
+		if (vector[i].nrUsi == nrUsi) {
+			suma += vector[i].pret;
+			index++;
+		}
+	}
+	if (index > 0) {
+		return suma / index;
+	}
+
+	return 0;
+}
+
+int main() {
+
+	Masina* masini = NULL;
+	int nrMasini = 0;
+	masini = citireVectorMasiniFisier("masini.txt", &nrMasini);
+	afisareVectorMasini(masini, nrMasini);
+
+	float medie = pretMediuDupaNrUsi(masini, nrMasini, 5);
+	printf("media masinilor este: %.2f", medie);
+
+	dezalocareVectorMasini(&masini, &nrMasini);
+
+	return 0;
+}
